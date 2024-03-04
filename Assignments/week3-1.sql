3dd6b4c712ea6733eb2741853390411d70267a40
ViragSzabo <virag.szabo@student.nhlstenden.com>
2024-03-04 15:25:07 +0100

    Update test data

--- Assignments/week3-1.sql
+++ Assignments/week3-1.sql
@@ -1,21 +1,22 @@
 -- Add test data for the AppUser table
-INSERT INTO dbo.AppUser (Username, Email, Password, SubscriptionID)
-VALUES 
-('DrSheldonCooper', 'sheldon.cooper@gmail.com', '32trainNerD49', 2),
-('LeonardHofstadter', 'leonard.hofstadter@gmail.com', 'Kal-el', 2),
-('WizardWolowitz', 'wizard.wolowitz@gmail.com', 'Wolowitz-0231', 2),
-('RajKoothrappali', 'raj.koothrappali@gmail.com', 'StarS304Beyonce', 1);
+INSERT INTO dbo.Subscription (Name, Price)
+VALUES
+('Single', 3.0),
+('Family', 2.0);
 
 INSERT INTO dbo.Artist (Name, Description)
 VALUES
 ('Taylor Swift', 'An artist of Pop.'),
 ('MIKA', 'An artist of Dance Pop.');
 
-INSERT INTO dbo.Subscription (Name, Price)
-VALUES
-('Single', 3.0),
-('Family', 2.0);
+INSERT INTO dbo.AppUser (Username, Email, Password, SubscriptionID)
+VALUES 
+('DrSheldonCooper', 'sheldon.cooper@gmail.com', '32trainNerD49', 16),
+('LeonardHofstadter', 'leonard.hofstadter@gmail.com', 'Kal-el', 16),
+('WizardWolowitz', 'wizard.wolowitz@gmail.com', 'Wolowitz-0231', 16),
+('RajKoothrappali', 'raj.koothrappali@gmail.com', 'StarS304Beyonce', 15);
 
 -- Verify the inserted data
 SELECT * FROM dbo.AppUser
 SELECT * FROM dbo.Artist
+SELECT * FROM dbo.Subscription 
