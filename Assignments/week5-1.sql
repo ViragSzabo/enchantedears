0f27919ff95b150a0b86f7619a6f7754dbe07d61
ViragSzabo <virag.szabo@student.nhlstenden.com>
2024-03-05 15:58:13 +0100

    Update

--- Assignments/week5-1.sql
+++ Assignments/week5-1.sql
@@ -3,14 +3,15 @@ GO
 
 -- Full Backup
 BACKUP DATABASE enchantedears
-TO DISK = 'C:\Users\szabo\OneDrive\Dokumentumok\University\Database2\EnchantedEars Final Assignment\enchantedears\Backup\enchantedears_Full.bak'
+TO DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup\enchantedears.bak'
 WITH INIT;
 
--- Differential Backup
+-- Differential Backup (Assuming a recent full backup)
 BACKUP DATABASE enchantedears
-TO DISK = 'C:\Users\szabo\OneDrive\Dokumentumok\University\Database2\EnchantedEars Final Assignment\enchantedears\Backup\enchantedears_Differential.bak'
+TO DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup\enchantedears_Differential.bak'
 WITH DIFFERENTIAL;
 
--- Incremental Backup
+-- Incremental Backup (Not needed in SIMPLE recovery model)
+-- SKIP this if you are not in the FULL recovery model
 BACKUP LOG enchantedears
-TO DISK = 'C:\Users\szabo\OneDrive\Dokumentumok\University\Database2\EnchantedEars Final Assignment\enchantedears\Backup\enchantedears_Incremental.trn';
+TO DISK = 'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup\enchantedears_Incremental.trn';
